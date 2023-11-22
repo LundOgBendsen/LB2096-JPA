e637609206b7061fd80b82cfa28296c1e323a5bf
Sinisa <sinisa_neskovic@yahoo.com>
2023-11-22 20:34:37 +0100

    add JPA-Ex05-Bi-directionalOneToMany

---
+++ Examples/JPA-Ex05-Bi-directionalOneToMany/README.txt
@@ -0,0 +1,3 @@
+JPA-Ex05-Bi-directionalOneToMany
+
+Built using maven and latest IntelliJ version.
---
+++ Examples/JPA-Ex05-Bi-directionalOneToMany/src/main/java/lungogbendsen/model/Car.java
@@ -0,0 +1,55 @@
+package lungogbendsen.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Car {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String make;
+
+	@ManyToOne
+	private Person owner;
+	
+	public Car() {
+	}
+
+	public Car(String make) {
+		this.make = make;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	// This method is only used in Person.addOwnedCar(Car)
+	void setOwner(Person person) {
+		this.owner = person;
+	}
+	
+	public Person getOwner() {
+		return owner;
+	}
+	
+	@Override
+	public String toString() {
+		String msg = "Car[id=" + id + ",make=" + this.make;
+		msg += ",ownerId=" + (getOwner() != null ? getOwner().getId() : null) + "]";
+		return msg;
+	}
+
+}

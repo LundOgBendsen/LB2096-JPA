c76d4400bc68832d3d040e3edaface2dde5d4cdc
Sinisa <sinisa_neskovic@yahoo.com>
2023-11-23 11:24:19 +0100

    add JPA-Ex26-SchemaGeneration - it works

---
+++ Examples/JPA-Ex26-SchemaGeneration/src/main/java/lundogbendsen/client/StandaloneJpaClient.java
@@ -0,0 +1,100 @@
+package lundogbendsen.client;
+
+import dk.lundogbendsen.jpa.util.JpaUtil;
+import dk.lundogbendsen.string.util.StringUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.RollbackException;
+import lundogbendsen.model.*;
+import org.apache.log4j.Level;
+
+public class StandaloneJpaClient {
+
+	public static void main(String[] args) {
+
+		JpaUtil.HibernateLoggingConfig loggingConfig = new JpaUtil.HibernateLoggingConfig();
+		loggingConfig.setHibernateRootLogLevel(Level.WARN);
+		loggingConfig.setLogDdlEnabled(true);
+		loggingConfig.setLogSqlEnabled(false);
+		loggingConfig.setLogParameterBindingEnabled(false);
+		JpaUtil.configureHibernateLogging(loggingConfig);
+		
+		// ///////////////////////////////////////////////////////////////////////
+		// Create new JPA entity manager (similar to JDBC Connection)
+		// JPA supports AutoCloseable, so we use try-with-resources
+		try(EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("StandaloneJpaTestPersistenceUnit");
+			EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+			exerciseWithJPA(loggingConfig, entityManagerFactory, entityManager);
+		}
+	}
+
+	public static void exerciseWithJPA(JpaUtil.HibernateLoggingConfig loggingConfig,
+									   EntityManagerFactory entityManagerFactory, EntityManager entityManager){
+
+		entityManager.getTransaction().begin();
+		createManyEntities(entityManager);
+		entityManager.getTransaction().commit();
+
+		entityManager.getTransaction().begin();
+		printAllEntities(entityManager);
+		entityManager.getTransaction().commit();
+	}
+
+	private static void createManyEntities(EntityManager entityManager) {
+		Person thomas = new Person("Thomas");
+		entityManager.persist(thomas);
+		Person lise = new Person("Lise");
+		entityManager.persist(lise);
+		Person johan = new Person("Johan");
+		entityManager.persist(johan);
+		Person sidsel = new Person("Sidsel");
+		entityManager.persist(sidsel);
+
+		Role thomasInstructorRole = new InstructorRole(thomas, new Period(1998, 4, 12), InstructorRole.Seniority.SENIOR);
+		entityManager.persist(thomasInstructorRole);
+		Role lise_instructorRole = new InstructorRole(lise, new Period(2005, 1, 1), InstructorRole.Seniority.JUNIOR);
+		entityManager.persist(lise_instructorRole);
+
+		Role lise_studentRole = new StudentRole(lise, new Period(2005, 1, 1));
+		entityManager.persist(lise_studentRole);
+		Role johan_studentRole = new StudentRole(johan, new Period(2004, 23, 5));
+		entityManager.persist(johan_studentRole);
+		Role sidsel_studentRole = new StudentRole(sidsel, new Period(2006, 12, 11));
+		entityManager.persist(sidsel_studentRole);
+
+		CourseType oop = new CourseType("LB1378", "Objektorienteret programmering med Java 5", 5);
+		entityManager.persist(oop);
+		CourseType ejb = new CourseType("LB1898", "Udvikling af Enterprise JavaBeans, EJB 3.0", 5);
+		entityManager.persist(ejb);
+		CourseType spring = new CourseType("LB2091", "The Spring Framework", 3);
+		entityManager.persist(spring);
+
+		Course oopSeptember = oop.createCourseInstance(2009, 9, 9);
+		oopSeptember.setInstructor(lise);
+		oopSeptember.addStudent(johan);
+		oopSeptember.addStudent(sidsel);
+		entityManager.persist(oopSeptember);
+
+		Course ejbOktober = ejb.createCourseInstance(2009, 10, 12);
+		ejbOktober.setInstructor(thomas);
+		ejbOktober.addStudent(lise);
+		ejbOktober.addStudent(johan);
+		entityManager.persist(ejbOktober);
+
+		Course ejbNovember = ejb.createCourseInstance(2009, 11, 29);
+		ejbNovember.addStudent(sidsel);
+		entityManager.persist(ejbNovember);
+	}
+
+	private static void printAllEntities(EntityManager entityManager) {
+		String jpaql = "select p from Person p";
+		JpaUtil.prettyPrintQueryResult(jpaql, entityManager.createQuery(jpaql).getResultList());
+		jpaql = "select r from Role r";
+		JpaUtil.prettyPrintQueryResult(jpaql, entityManager.createQuery(jpaql).getResultList());
+		jpaql = "select ct from CourseType ct";
+		JpaUtil.prettyPrintQueryResult(jpaql, entityManager.createQuery(jpaql).getResultList());
+		jpaql = "select c from Course c";
+		JpaUtil.prettyPrintQueryResult(jpaql, entityManager.createQuery(jpaql).getResultList());
+	}
+}
---
+++ Examples/JPA-Ex26-SchemaGeneration/src/main/java/lundogbendsen/model/Course.java
@@ -0,0 +1,155 @@
+package lundogbendsen.model;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({ 
+	@NamedQuery(name = "Course.findAll", query = "SELECT c FROM Course c"),
+	@NamedQuery(name = "Course.findAllIds", query = "SELECT c.id FROM Course c") 
+})
+public class Course {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private Period period;
+
+	@ManyToOne
+	private CourseType courseType;
+
+	@ManyToMany
+	private Collection<StudentRole> students = new LinkedList<StudentRole>();
+
+	@ManyToOne
+	private InstructorRole instructor;
+
+	@SuppressWarnings("unused")
+	private Course() {
+		// Must have no-args constructor
+	}
+
+	public Course(CourseType courseType, Period period) {
+		if (courseType == null) {
+			throw new NullPointerException("Parameter [courseType] must not be null");
+		}
+		if (period == null) {
+			throw new NullPointerException("Parameter [period] must not be null");
+		}
+		this.courseType = courseType;
+		this.period = period;
+	}
+
+	public void setInstructor(Person instructor) {
+		Collection<Role> assignedRoles = instructor.getAssignedRoles();
+		InstructorRole instructorRole = null;
+		for (Role role : assignedRoles) {
+			if (role instanceof InstructorRole) {
+				instructorRole = (InstructorRole) role;
+				break;
+			}
+		}
+		if (instructorRole == null) {
+			String msg = "The person [" + instructor + "] is not assigned an instructor role";
+			throw new IllegalArgumentException(msg);
+		}
+		if (this.instructor != null) {
+			this.instructor.removeCourse(this);
+		}
+		instructorRole.addCourse(this);
+		this.instructor = instructorRole;
+	}
+
+	public void addStudent(Person student) {
+		Collection<Role> assignedRoles = student.getAssignedRoles();
+		StudentRole studentRole = null;
+		for (Role role : assignedRoles) {
+			if (role instanceof StudentRole) {
+				studentRole = (StudentRole) role;
+				break;
+			}
+		}
+		if (studentRole == null) {
+			String msg = "The person [" + student + "] is not assigned a student role";
+			throw new IllegalArgumentException(msg);
+		}
+		studentRole.addCourse(this);
+		this.students.add(studentRole);
+	}
+
+	public InstructorRole getInstructor() {
+		return instructor;
+	}
+
+	public Period getPeriod() {
+		return period;
+	}
+
+	public CourseType getCourseType() {
+		return courseType;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Collection<StudentRole> getStudents() {
+		return Collections.unmodifiableCollection(students);
+	}
+
+	public String getInstructorName() {
+		if (this.instructor == null) {
+			return "UNKNOWN";
+		} else {
+			Person person = this.instructor.getPersonAssignedToRole();
+			if (person != null) {
+				return person.getName();
+			} else {
+				return "UNKNOWN";
+			}
+		}
+	}
+
+	public String getCourseNumber() {
+		if (this.courseType != null) {
+			return this.courseType.getNumber();
+		} else {
+			return null;
+		}
+	}
+
+	public List<String> getNamesOfStudents() {
+		List<String> studentNames = new LinkedList<String>();
+		for (StudentRole studentRole : this.students) {
+			Person personAssignedToRole = studentRole.getPersonAssignedToRole();
+			String studentName = null;
+			if (personAssignedToRole != null) {
+				studentName = personAssignedToRole.getName();
+			}
+			studentNames.add(studentName == null ? "UNKNOWN" : studentName);
+		}
+		return studentNames;
+	}
+
+	@Override
+	public String toString() {
+		String s = "Course[";
+		s += "courseNumber=" + getCourseNumber();
+		s += ",period=" + this.period;
+		s += ",instructorName=" + getInstructorName();
+		s += ",namesOfStudents=" + getNamesOfStudents();
+		s += "]";
+		return s;
+	}
+}
